e6d98b9d5b3a6c65146ef64c22344cf5cf32b2d7
Ntobeko Dindi <ntobeko.dindi@dutappfactory.co.za>
2022-10-30 00:55:51 +0200

    added login and signup screens

--- app/src/main/java/com/ntobeko/confmanagement/ui/home/HomeFragment.java
+++ app/src/main/java/com/ntobeko/confmanagement/ui/home/HomeFragment.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.ntobeko.confmanagement.R;
 import com.ntobeko.confmanagement.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -24,8 +25,29 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        binding.singUp.setOnClickListener(v -> {
+            binding.logInLayout.setVisibility(View.GONE);
+            binding.singUpLayout.setVisibility(View.VISIBLE);
+            binding.singUp.setBackgroundResource(R.drawable.switch_trcks);
+            binding.singUp.setTextColor(getResources().getColor(R.color.white));
+            binding.logIn.setBackgroundResource(R.drawable.switch_tumbs_null);
+            binding.logIn.setTextColor(getResources().getColor(R.color.pinkColor));
+            binding.singIn.setText(getResources().getText(R.string.sign_up));
+        });
+
+        binding.logIn.setOnClickListener(v -> {
+            binding.logInLayout.setVisibility(View.VISIBLE);
+            binding.singUpLayout.setVisibility(View.GONE);
+            binding.logIn.setBackgroundResource(R.drawable.switch_trcks);
+            binding.logIn.setTextColor(getResources().getColor(R.color.white));
+            binding.singUp.setBackgroundResource(R.drawable.switch_tumbs_null);
+            binding.singUp.setTextColor(getResources().getColor(R.color.pinkColor));
+            binding.singIn.setText(getResources().getText(R.string.log_in));
+        });
+
+        binding.singIn.setOnClickListener(v -> {
+            Toast.makeText(getContext(), "Logging in...", Toast.LENGTH_LONG).show();
+        });
         return root;
     }
 
