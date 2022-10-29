72def86357ff77672b5b3f0d29835515b0ab382b
Ntobeko Dindi <ntobeko.dindi@dutappfactory.co.za>
2022-10-29 21:35:50 +0200

    init

---
+++ app/src/main/java/com/ntobeko/confmanagement/ui/news/NewsViewModel.java
@@ -0,0 +1,19 @@
+package com.ntobeko.confmanagement.ui.news;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NewsViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public NewsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
