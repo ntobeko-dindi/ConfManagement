10df068a8bc1e5c5894be798a1893fe5fba9dd14
Ntobeko Dindi <ntobeko.dindi@dutappfactory.co.za>
2022-11-12 13:50:35 +0200

    added auth activity and done some code refactoring

---
+++ app/src/main/java/com/ntobeko/confmanagement/ui/login/LoginFragment.java
@@ -0,0 +1,63 @@
+package com.ntobeko.confmanagement.ui.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.ntobeko.confmanagement.AuthActivity;
+import com.ntobeko.confmanagement.R;
+import com.ntobeko.confmanagement.databinding.FragmentLoginBinding;
+
+public class LoginFragment extends Fragment {
+
+    private FragmentLoginBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        LoginViewModel homeViewModel =
+                new ViewModelProvider(this).get(LoginViewModel.class);
+
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        binding.singUp.setOnClickListener(v -> {
+            binding.logInLayout.setVisibility(View.GONE);
+            binding.singUpLayout.setVisibility(View.VISIBLE);
+            binding.singUp.setBackgroundResource(R.drawable.switch_trcks);
+            binding.singUp.setTextColor(getResources().getColor(R.color.white));
+            binding.logIn.setBackgroundResource(R.drawable.switch_tumbs_null);
+            binding.logIn.setTextColor(getResources().getColor(R.color.pinkColor));
+            binding.singIn.setText(getResources().getText(R.string.sign_up));
+        });
+
+        binding.logIn.setOnClickListener(v -> {
+            binding.logInLayout.setVisibility(View.VISIBLE);
+            binding.singUpLayout.setVisibility(View.GONE);
+            binding.logIn.setBackgroundResource(R.drawable.switch_trcks);
+            binding.logIn.setTextColor(getResources().getColor(R.color.white));
+            binding.singUp.setBackgroundResource(R.drawable.switch_tumbs_null);
+            binding.singUp.setTextColor(getResources().getColor(R.color.pinkColor));
+            binding.singIn.setText(getResources().getText(R.string.log_in));
+        });
+
+        binding.singIn.setOnClickListener(v -> {
+            Intent i = new Intent(getContext(), AuthActivity.class);
+            startActivity(i);
+            getActivity().finish();
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
