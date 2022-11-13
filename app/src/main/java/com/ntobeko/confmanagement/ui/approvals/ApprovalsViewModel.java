2e1e68797ee2c38b8e06808d3d0606bfb14aeac9
Ntobeko Dindi <ntobeko.dindi@dutappfactory.co.za>
2022-11-13 14:31:35 +0200

    renamed nav drawer items

---
+++ app/src/main/java/com/ntobeko/confmanagement/ui/approvals/ApprovalsViewModel.java
@@ -0,0 +1,19 @@
+package com.ntobeko.confmanagement.ui.approvals;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ApprovalsViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public ApprovalsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
