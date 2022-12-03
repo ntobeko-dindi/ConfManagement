80028de7312ec03b58b09ccef98cb96659560b30
Ntobeko Dindi <ntobeko.dindi@dutappfactory.co.za>
2022-12-03 23:40:43 +0200

    few changes

--- app/src/main/java/com/ntobeko/confmanagement/models/LocalDate.java
+++ app/src/main/java/com/ntobeko/confmanagement/models/LocalDate.java
@@ -7,7 +7,7 @@ public class LocalDate {
 
     public String getLocalDateTime(){
         LocalDateTime localDateTime = LocalDateTime.now();
-        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return localDateTime.format(myFormatObj);
     }
 }
