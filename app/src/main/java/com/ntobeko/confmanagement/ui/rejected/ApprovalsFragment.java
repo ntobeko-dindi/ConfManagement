70b6af9e6f2f764012391759aaca2ecc1b7717df
Ntobeko Dindi <ntobeko.dindi@dutappfactory.co.za>
2022-12-03 04:40:49 +0200

    added approval history

---
+++ app/src/main/java/com/ntobeko/confmanagement/ui/rejected/ApprovalsFragment.java
@@ -0,0 +1,35 @@
+package com.ntobeko.confmanagement.ui.rejected;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.ntobeko.confmanagement.Enums.ProposalStatus;
+import com.ntobeko.confmanagement.data.FireBaseHelper;
+import com.ntobeko.confmanagement.databinding.FragmentApprovalsBinding;
+
+public class ApprovalsFragment extends Fragment {
+
+    private FragmentApprovalsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentApprovalsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        new FireBaseHelper().getAbstractsPendingApprovals(root,getContext(),binding, getActivity(), ProposalStatus.Rejected);
+        //new FireBaseHelper().getConferenceAttendeePendingApprovals(root,getContext(),binding);
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
